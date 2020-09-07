0a7f0aa9e693d829b3e61a32239e90096c0f6057
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2020-09-07 14:59:04 +0300

    added onSaveInstanceState

--- app/src/main/java/com/example/quizapp/QuizMain.java
+++ app/src/main/java/com/example/quizapp/QuizMain.java
@@ -1,10 +1,13 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,9 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class QuizMain extends AppCompatActivity {
+
+private static  final String TAG = "QuizMain";
+private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -38,7 +44,12 @@ public class QuizMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"OnCreate(Bundle) Called ");
         setContentView(R.layout.activity_quiz);
+
+        if( savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mTrueButton = (Button)findViewById(R.id.trueButton);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -71,11 +82,10 @@ public class QuizMain extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQestionBank.length;
-                if ( mCurrentIndex == 0){
-                    mCurrentIndex = 1;
-                }
-                mCurrentIndex = 0 ;
+                mCurrentIndex = mCurrentIndex - 1 ;
+            if (mCurrentIndex == -1){
+                mCurrentIndex = 5;
+            }
                 UpdateQuestion();
             }
         });
@@ -112,5 +122,43 @@ public class QuizMain extends AppCompatActivity {
             massegeResId = R.string.toastIncorrect_text;
         }
         Toast.makeText(this,massegeResId,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"OnStart() Called ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"OnResume() Called ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"OnPause() Called ");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"OnStop() Called ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"OnDestroy() Called ");
     }
 }
