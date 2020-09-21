952cf7a9cf155f1dd1712458912d3bb9d9eff587
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2020-09-21 20:57:38 +0300

    added cheat Button

--- app/src/main/java/com/example/quizapp/CheatActivity.java
+++ app/src/main/java/com/example/quizapp/CheatActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CheatActivity extends AppCompatActivity {
 
 private static final String EXTRA_ANSWER_IS_TRUE = "com.example.quizapp.anwser_is_true";
 
 private  boolean mAnswerIsTrue;
+private TextView mAnswerTextView;
+private Button mShowAnswerButton;
 
 public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent intent =  new Intent(packageContext, CheatActivity.class);
@@ -22,8 +29,23 @@ public static Intent newIntent(Context packageContext, boolean answerIsTrue){
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
-
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
+
+
 
 
     }
